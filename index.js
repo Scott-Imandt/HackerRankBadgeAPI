573f3f25159b41ec0c7bf977c29ac9c1b12424d2
Scott Imandt <scottimandt@gmail.com>
2024-11-12 20:42:32 -0500

    Manually set Cors differently

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
